2a71dbeade77f92e08e056b11fb10e9454bf50df
Emre Basarir <yunu_emre_kardes@outlook.com>
2022-09-17 18:04:09 +0300

    Two commint

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Atama ve İşlemli Atama*****");
+            // Atama ve İşlemli Atama
+            int y = 3;
+
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 2;
+            Console.WriteLine(y);
+
+            int x = 3;
+            x *= 2;
+            Console.WriteLine(x);
+
+            Console.WriteLine("*****Mantıksal Operatörler*****");
+            // Mantıksal Operatörler
+            // ||, &&, !
+            
+            bool isSucces = true;
+            bool isCompletad = false;
+
+            if(isSucces && isCompletad)
+            Console.WriteLine("Perfect!");
+
+             if(isSucces || isCompletad)
+            Console.WriteLine("Great!");
+
+             if(isSucces && !isCompletad)
+            Console.WriteLine("Fine!");
+
+
+            Console.WriteLine("*****İlişkisel Operatörler*****");
+            // İlişkisel Operatörler
+            // <, >, <=, >=, ==, !=
+
+            int a = 3;
+            int b = 4;
+
+            bool sonuc = a<b;
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc = a<=b;
+            Console.WriteLine(sonuc);
+            sonuc = a>=b;
+            Console.WriteLine(sonuc);
+            sonuc = a==b;
+            Console.WriteLine(sonuc);
+            sonuc = a!=b;
+            Console.WriteLine(sonuc);
+
+
+            Console.WriteLine("*****Aritmatik Operatörler*****");
+            // Aritmatik  Operatörler
+            // /, *, +,-
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1-sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1*sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1/sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = ++sayi1; 
+            Console.WriteLine(sonuc1);
+
+
+            // % Mod Alma
+            int sonuc3 = 20%3;
+            Console.WriteLine(sonuc3 );
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+[C# Operatörler](https://app.patika.dev/courses/csharp-101/5-operatorler)
